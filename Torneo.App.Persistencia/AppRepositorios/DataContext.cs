fce2e49b40b9bfa932d154f358f5af107d7cde76
Yhosi Dulcey <yhesteban17@gmail.com>
2022-09-26 19:42:36 -0500

    actualizado estadios

--- Torneo.App.Persistencia/AppRepositorios/DataContext.cs
+++ Torneo.App.Persistencia/AppRepositorios/DataContext.cs
@@ -18,8 +18,8 @@ namespace Torneo.App.Persistencia
             if (!optionsBuilder.IsConfigured)
             {
 
-                //optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Torneo2");
-                optionsBuilder.UseSqlServer("Server=tcp:misionticronin.database.windows.net,1433;Initial Catalog=Torneo2;Persist Security Info=False;User ID=admin_1;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;EnableRetryOnFailure");
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Torneo2");
+                //optionsBuilder.UseSqlServer("Server=tcp:misionticronin.database.windows.net,1433;Initial Catalog=Torneo2;Persist Security Info=False;User ID=admin_1;Password=abcd1234*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;EnableRetryOnFailure");
                 //optionsBuilder.UseSqlServer("Data Source = MSI\\SQLEXPRESS; Initial Catalog = Torneo2; Trusted_Connection=True");
             }
         }
